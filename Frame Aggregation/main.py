28736cb115d464289fa39a13c01aed76f1c19454
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 08:54:45 +0000

    Edited 'main.py'

--- Frame Aggregation/main.py
+++ Frame Aggregation/main.py
@@ -5,7 +5,7 @@ from quixstreams import Application
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
+app = Application(consumer_group="transformation-v2", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
@@ -30,7 +30,7 @@ window = (
 )
 
 sdf.print()
-sdf.to_topic(output_topic)
+# sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
