b7704a2b83b792d94c747c83cbdbb25f4365f36c
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 10:00:25 +0000

    Edited 'main.py'

--- Scoring/main.py
+++ Scoring/main.py
@@ -12,9 +12,20 @@ output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+def should_skip(data):
+    return data.get('length')
+
+sdf = (
+    sdf.filter(should_skip)
+    # Extract "temperature" value from the message
+    .apply(lambda value: value["score"])
+    # You can also pass duration_ms as an integer of milliseconds
+    .tumbling_window(duration_ms=5000)
+    # Specify the "mean" aggregate function
+    .mean()
+    # Emit updates for each incoming message
+    .current()
+)
 
 sdf.print()
 sdf.to_topic(output_topic)
