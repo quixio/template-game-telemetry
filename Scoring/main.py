667c9bae78c11eb599df7df597a531c7fe467374
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 14:33:04 +0000

    Edited 'main.py'

--- Scoring/main.py
+++ Scoring/main.py
@@ -1,58 +1,34 @@
 import os
 from quixstreams import Application, State
-from datetime import timedelta
-import pandas as pd
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v54", auto_offset_reset="earliest")
+app = Application(consumer_group='transformation-v54', auto_offset_reset='earliest')
 
-input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"])
+input_topic = app.topic(os.environ['input'])
+output_topic = app.topic(os.environ['output'])
 
 sdf = app.dataframe(input_topic)
 
 def can_process(data):
     return data.get('type') and data.get('snakeLength')
 
-
-def initializer(value: dict) -> dict:
-    return {
-        #'id': value['session_id'],
-        'score': 0
-    }
-
-
-def reducer(aggregated: dict, value: dict) -> dict:
-
-    score = aggregated['score']
-
-    if value['type'] == 'apple-eaten':
-        score += 1
-    
-    return {
-        #'session_id': value['session_id'],
-        'score': score
-    }
+sdf = sdf.filter(can_process)
 
 def calc_score(data: dict, state: State):
     score = state.get('score', 0)
     if data['type'] == 'apple-eaten':
         score += 1
-        # print("Apple eaten..")
     state.set('score', score)
     data['score'] = score
 
-
-
-sdf = sdf.filter(can_process)
 sdf = sdf.update(calc_score, stateful=True)
 
 def score_json(rows):
     data = {
-        'score': rows["score"],
+        'score': rows['score'],
         'length': rows['snakeLength']
     }
     return data
@@ -60,23 +36,7 @@ def score_json(rows):
 sdf = sdf.apply(score_json)
 sdf = sdf.update(lambda row: print(row))
 
-# sdf = (
-#     sdf.filter(should_skip)
-#     # Extract "temperature" value from the message
-#     .apply(lambda value: 1)
-#     # You can also pass duration_ms as an integer of milliseconds
-#     .tumbling_window(duration_ms=5000)
-#     # Specify the "sum" aggregate function
-#     .sum()
-#     # Emit updates for each incoming message
-#     .current()
-# )
-
-# sdf = [sdf["value"]]
-
-# sdf.print()
-
 sdf.to_topic(output_topic)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(sdf)
