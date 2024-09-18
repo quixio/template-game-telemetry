6de6964b705ef53d58f286ad6c50abdad14c9921
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 15:27:07 +0000

    Edited 'main.py'

--- redis-test/main.py
+++ redis-test/main.py
@@ -1,20 +1,18 @@
 import redis
+import json
 
-def test_redis():
-    # Connect to Redis server
-    client = redis.Redis(host='redis', port=6379, db=0)
+# Connect to Redis
+client = redis.Redis(host='redis', port=6379, decode_responses=True)
 
-    # Test setting a key
-    client.set('test_key', 'test_value')
+# Get all keys from Redis
+keys = client.keys('*')
 
-    # Test getting the key
-    value = client.get('test_key').decode('utf-8')
-
-    # Print the result
-    print(f"Value for 'test_key': {value}")
-
-    # Clean up
-    client.delete('test_key')
-
-if __name__ == "__main__":
-    test_redis()
+# Print all keys and their values
+for key in keys:
+    value = client.get(key)
+    try:
+        # Try to parse the value as JSON
+        value = json.loads(value)
+    except json.JSONDecodeError:
+        pass  # If it's not JSON, just keep the value as is
+    print(f"{key}: {value}")
