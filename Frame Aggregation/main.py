471e9a68f422e345fb14ddd556901baf6d8c7b1d
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 09:10:38 +0000

    Edited 'main.py'

--- Frame Aggregation/main.py
+++ Frame Aggregation/main.py
@@ -5,7 +5,11 @@ from quixstreams import Application
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v4", auto_offset_reset="earliest")
+from uuid import uuid4
+
+xxx = str(uuid4())
+
+app = Application(consumer_group="transformation-v4" + xxx, auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
