3867fc070392c694dc982ae399d20d5b71c34314
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 08:42:13 +0000

    Edited 'main.py'

--- Frame Aggregation/main.py
+++ Frame Aggregation/main.py
@@ -12,9 +12,23 @@ output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+# Using "reduce()" to calculate multiple aggregates at once
+def reducer(agg: dict, current: int):
+    aggregated = {
+        'min': min(agg['min'], current),
+        'max': max(agg['max'], current),
+        'count': agg['count'] + 1
+    }
+    return aggregated
+
+def initializer(current) -> dict:
+    return {'min': current, 'max': current, 'count': 1}
+
+window = (
+    sdf.tumbling_window(duration_ms=1000)
+    .reduce(reducer=reducer, initializer=initializer)
+    .final()
+)
 
 sdf.print()
 sdf.to_topic(output_topic)
