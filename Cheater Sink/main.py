04a8156fbb924419e2707c7daed8c7b3010e14db
Steve Rosam <steve+quix-org@quix.io>
2024-09-18 15:20:28 +0000

    Edited 'main.py'

--- Cheater Sink/main.py
+++ Cheater Sink/main.py
@@ -23,11 +23,11 @@ app = Application(consumer_group="redis-destination")
 input_topic = app.topic(os.environ["input"])
 
 
-def send_data_to_redis(value: dict, a, b, c) -> None:
+def send_data_to_redis(value: dict, key) -> None:
     print(value)
-    print(a)
-    print(b)
-    print(c)
+    print("--")
+    print(key)
+    print("--")
     # Convert the entire dictionary to a JSON string
     json_data = json.dumps(value)
 
