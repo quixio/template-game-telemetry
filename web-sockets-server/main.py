930cf18401e6049438e387e331076800486ca6dd
Steve Rosam <steve+quix-org@quix.io>
2024-09-19 11:04:57 +0000

    Edited 'main.py'

--- web-sockets-server/main.py
+++ web-sockets-server/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 class webSocketSource:
     
     def __init__(self) -> None:
-        app = Application.Quix("web-sockets-server-v100", auto_offset_reset="latest", loglevel: 'DEBUG')
+        app = Application.Quix("web-sockets-server-v100", auto_offset_reset="latest", loglevel= 'DEBUG')
         self._topic = app.topic(name=os.environ["input"])
         
         self._consumer = app.get_consumer()
