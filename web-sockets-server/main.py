45129b69dd60f95db8475b45cf50f50f1d107ae3
Steve Rosam <steve+quix-org@quix.io>
2024-09-19 10:11:20 +0000

    Edited 'main.py'

--- web-sockets-server/main.py
+++ web-sockets-server/main.py
@@ -15,8 +15,8 @@ class webSocketSource:
         self._topic = app.topic(name=os.environ["input"])
         
         self._consumer = app.get_consumer()
-        # self._consumer.subscribe([self._topic.name])
-        self._consumer.subscribe(["score"])
+        self._consumer.subscribe([self._topic.name])
+        # self._consumer.subscribe(["score"])
 
         # Holds all client connections partitioned by page.
         self.websocket_connections = {}
