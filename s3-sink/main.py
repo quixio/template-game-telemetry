0f8e59201dcdc25f73e8127313a88b52cb540608
Tomas Neubauer <tomas@quix.ai>
2024-09-24 14:14:28 +0200

    fix

--- s3-sink/main.py
+++ s3-sink/main.py
@@ -12,8 +12,8 @@ app = Application(consumer_group="destination-v2.17",
 input_topic = app.topic(os.environ["input"])
 
 output_s3 = S3Sink(
-    os.environ["AWS_S3_URI"],
     os.environ["table_name"],
+    os.environ["AWS_S3_URI"],
     s3_region_name=os.environ["AWS_DEFAULT_REGION"],)
 
 sdf = app.dataframe(input_topic)
