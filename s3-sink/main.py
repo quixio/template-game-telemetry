e4cdb2ea0e424dcf4643ce43022b97126a1af3ca
Tomas Neubauer <tomas@quix.ai>
2024-09-24 14:31:45 +0200

    Docs

--- s3-sink/main.py
+++ s3-sink/main.py
@@ -1,5 +1,5 @@
 from quixstreams import Application
-from s3_sink import S3Sink
+from iceberg_sink import IcebergSink
 import os
 
 from dotenv import load_dotenv
@@ -11,13 +11,13 @@ app = Application(consumer_group="destination-v2.17",
 
 input_topic = app.topic(os.environ["input"])
 
-output_s3 = S3Sink(
+iceberg_sink = IcebergSink(
     os.environ["table_name"],
     os.environ["AWS_S3_URI"],
     s3_region_name=os.environ["AWS_DEFAULT_REGION"],)
 
 sdf = app.dataframe(input_topic)
-sdf.sink(output_s3)
+sdf.sink(iceberg_sink)
 
 if __name__ == "__main__":
     app.run(sdf)
