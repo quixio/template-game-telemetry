55f6e187d129824d5310fe8660f7ca25b2ed8cdb
Tomas Neubauer <tomas@quix.io>
2024-10-09 10:13:16 +0000

    Edited 'main.py'

--- xml-transform/main.py
+++ xml-transform/main.py
@@ -1,14 +1,14 @@
 import os
 from quixstreams import Application
-from xml_serializer import XMLSerializer
+from xml_serializer import XMLSerializer, XM
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Application(consumer_group="xml-v1", auto_offset_reset="earliest")
 
-input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"], value_serializer=XMLSerializer())
+input_topic = app.topic(os.environ["output"], value_deserializer=X)
+#output_topic = app.topic(os.environ["output"], value_serializer=XMLSerializer())
 
 sdf = app.dataframe(input_topic)
 
