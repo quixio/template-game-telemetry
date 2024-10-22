4c8ec4a84ffc87c575f519d0b62278f986893f04
Steve Rosam <steve+newdemo@quix.io>
2024-10-22 09:11:15 +0000

    Edited 'main.py'

--- s3-sink/main.py
+++ s3-sink/main.py
@@ -5,22 +5,22 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 print(os.environ)
-app = Application(consumer_group="destination-v2.17", 
-                  auto_offset_reset = "earliest",
-                  commit_interval=5)
+# app = Application(consumer_group="destination-v2.17", 
+#                   auto_offset_reset = "earliest",
+#                   commit_interval=5)
 
-input_topic = app.topic(os.environ["input"])
+# input_topic = app.topic(os.environ["input"])
 
-iceberg_sink = IcebergSink(
-    data_catalog_spec="aws_glue",
-    table_name=os.environ["table_name"],
-    config=AWSIcebergConfig(
-        aws_s3_uri=os.environ["AWS_S3_URI"],
-        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"]))
+# iceberg_sink = IcebergSink(
+#     data_catalog_spec="aws_glue",
+#     table_name=os.environ["table_name"],
+#     config=AWSIcebergConfig(
+#         aws_s3_uri=os.environ["AWS_S3_URI"],
+#         aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+#         aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"]))
 
-sdf = app.dataframe(input_topic)
-sdf.sink(iceberg_sink)
+# sdf = app.dataframe(input_topic)
+# sdf.sink(iceberg_sink)
 
-if __name__ == "__main__":
-    app.run(sdf)
+# if __name__ == "__main__":
+#     app.run(sdf)
