8e1c0b60ebba0034b959931e97a568b28d81a414
Tomas Neubauer <tomas@quix.ai>
2024-10-30 15:45:44 +0100

    Azure iceberg

--- azure-iceberg-sink/formats.py
+++ azure-iceberg-sink/formats.py
@@ -1,17 +1,12 @@
 import abc
-import json
 from gzip import compress as gzip_compress
 from io import BytesIO
-from typing import Optional, Callable, Any, List
+from typing import Any, List
+from quixstreams.sinks.base import SinkBatch
 
-from jsonlines import Writer
 from datetime import datetime
 
-__all__ = ("S3SinkBatchFormat", "JSONFormat", "BytesFormat", "ParquetFormat")
-
-
-# TODO: Document the compatible topic formats for each formatter
-# TODO: Check the types of the values before serializing
+__all__ = ("S3SinkBatchFormat", "ParquetFormat")
 
 
 class S3SinkBatchFormat:
@@ -29,78 +24,12 @@ class S3SinkBatchFormat:
         ...
 
     @abc.abstractmethod
-    def serialize_batch_values(self, values: List[Any]) -> bytes:
+    def serialize_batch_values(self, values: List[Any]) -> List[bytes]:
         ...
 
 
-class BytesFormat(S3SinkBatchFormat):
-    """
-    Bypass formatter to serialize
-    """
-
-    def __init__(
-        self,
-        separator: bytes = b"\n",
-        file_extension: str = ".bin",
-        compress: bool = False,
-    ):
-        self._separator = separator
-        self._file_extension = file_extension
-        self._compress = compress
-        if self._compress:
-            self._file_extension += ".gz"
-
-    @property
-    def file_extension(self) -> str:
-        return self._file_extension
-
-    def deserialize_value(self, value: bytes) -> Any:
-        return value
-
-    def serialize_batch_values(self, values: List[bytes]) -> bytes:
-        value_bytes = self._separator.join(values)
-        if self._compress:
-            value_bytes = gzip_compress(value_bytes)
-        return value_bytes
-
-
-class JSONFormat(S3SinkBatchFormat):
-    # TODO: Docs
-    def __init__(
-        self,
-        dumps: Optional[Callable[[Any], bytes]] = None,
-        loads: Optional[Callable[[bytes], Any]] = None,
-        file_extension: str = ".json",
-        compress: bool = False,
-    ):
-        self._dumps = dumps or json.dumps
-        self._loads = loads or json.loads
-        self._compress = compress
-        self._file_extension = file_extension
-        if self._compress:
-            self._file_extension += ".gz"
-
-    @property
-    def file_extension(self) -> str:
-        return self._file_extension
-
-    def deserialize_value(self, value: bytes) -> Any:
-        # TODO: Wrap an exception with more info here
-        return self._loads(value)
-
-    def serialize_batch_values(self, values: List[Any]) -> bytes:
-        with BytesIO() as f:
-            with Writer(f, compact=True, dumps=self._dumps) as writer:
-                writer.write_all(values)
-            value_bytes = f.getvalue()
-            if self._compress:
-                value_bytes = gzip_compress(value_bytes)
-            return value_bytes
-        
-
 import pyarrow as pa
 import pyarrow.parquet as pq
-from io import BytesIO
 import gzip
 
 class ParquetFormat(S3SinkBatchFormat):
@@ -125,27 +54,19 @@ class ParquetFormat(S3SinkBatchFormat):
             table = pq.read_table(f)
             return table.to_pydict()
 
-    def serialize_batch_values(self, values: List[Any]) -> bytes:
-        # Get all unique keys (columns) across all rows
-        all_keys = set()
+    def serialize_batch_values(self, values: SinkBatch) -> tuple[str, bytes]:
+
+        columns = {}
+
+
         for row in values:
-            all_keys.update(row.value.keys())
+            for key in row.value:
+                if key not in columns:
+                    columns[key] = []
 
-        # Normalize rows: Ensure all rows have the same keys, filling missing ones with None
-        normalized_values = [
-            {key: row.value.get(key, None) for key in all_keys} for row in values
-        ]
-
-        columns = {
-            "_timestamp": [datetime.fromtimestamp(row.timestamp / 1000.0) for row in values], 
-            "_key": [bytes.decode(row.key) for row in values]
-        }
-
-        # Convert normalized values to a pyarrow Table
-        columns = {**columns, **{key: [row[key] for row in normalized_values] for key in all_keys}}
-                   
+                columns[key].append(row.value[key])
+                    
         table = pa.Table.from_pydict(columns)
-
         with BytesIO() as f:
             pq.write_table(table, f, compression=self._compression_type)
             value_bytes = f.getvalue()
--- azure-iceberg-sink/main.py
+++ azure-iceberg-sink/main.py
@@ -1,22 +1,65 @@
-from quixstreams import Application
+from quixstreams import Application, State
 from iceberg_sink import IcebergSink
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 load_dotenv()
-
-app = Application(consumer_group="destination-v2.17", 
+    
+app = Application(consumer_group=os.environ["consumer_group"], 
                   auto_offset_reset = "earliest",
-                  commit_interval=5)
+                  commit_interval=float(os.environ["commit_interval"]),
+                  commit_every=int(os.environ["commit_every"]))
 
-input_topic = app.topic(os.environ["input"])
+
+# We deserialize only messages with columns we need.
+input_topic = app.topic(os.environ["input"], key_deserializer="string", value_deserializer="json")
 
 iceberg_sink = IcebergSink(
-    os.environ["table_name"],
-    os.environ["AWS_S3_URI"],
-    s3_region_name=os.environ["AWS_DEFAULT_REGION"],)
+    postgres_connection_str=os.environ["POSTGRES_CONNECTION_STR"],
+    aws_s3_uri=os.environ["AZURE_STORAGE_URI"],
+    namespace=os.environ["NAMESPACE"],
+    table_name=os.environ["TABLE_NAME"],
+    data_catalog_spec="postgres"
+    )
+
 
 sdf = app.dataframe(input_topic)
+
+
+
+
+def aggregate_last_value(row: dict, state: State):
+    
+    last_values = state.get("last_values", {
+        "type": None,
+        "x": None,
+        "y": None,
+        "snakeLength": None,
+        "session_id": None,
+        "reason": None,
+        "key": None
+    })
+    
+    last_values = {
+        **last_values,
+        **row
+    }
+    
+    state.set("last_values", last_values)
+    
+    return last_values
+    
+    
+
+sdf = sdf.apply(aggregate_last_value, stateful=True)
+
+sdf = sdf.apply(lambda row, key, timestamp, headers: {
+    "_timestamp": datetime.fromtimestamp(timestamp/1000),
+    "_key": key,
+    **row
+}, metadata=True)
+sdf.print()
 sdf.sink(iceberg_sink)
 
 if __name__ == "__main__":
